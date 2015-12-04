93efba4ddebd69587db7a908cc783723b27147f8
Fredrick Khoury <fredrick.f.khoury@lowes.com>
2015-12-04 12:50:57 -0500

    Formatting

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ io.
 	on('connection', 
 		function(socket){
   			console.log('a user connected');
-			socket.
-				on('disconnect', 
+			socket
+        .on('disconnect', 
 					function(){
 						console.log('user disconnected');
   					})
-  				.on('chat message',
+        .on('chat message',
   					function(msg){
   						console.log('message: ' + msg);
   					});
